e88aed3585658dbeebfb1dd407d9c0712dbfa3b8
vigneshwaran48 <p3487260@gmail.com>
2025-03-19 21:57:44 +0530

    Added basic execution loop

---
+++ src/display.rs
@@ -0,0 +1,44 @@
+use sdl2::{EventPump, Sdl, VideoSubsystem, pixels::Color, render::Canvas, video::Window};
+
+pub struct Display {
+    context: Sdl,
+    video_system: VideoSubsystem,
+    canvas: Canvas<Window>,
+}
+
+impl Display {
+    pub fn init() -> Result<Self, String> {
+        let context =
+            sdl2::init().map_err(|e| format!("SDL context initialization failed: {}", e))?;
+        let video_system = context
+            .video()
+            .map_err(|e| format!("Failed to initialize video system: {}", e))?;
+
+        let window = video_system
+            .window("Chip 8", 800, 600)
+            .position_centered()
+            .opengl()
+            .build()
+            .map_err(|e| format!("Failed to create window: {}", e))?;
+
+        let mut canvas = window
+            .into_canvas()
+            .build()
+            .map_err(|e| format!("Failed to create canvas: {}", e))?;
+
+        // Set initial draw color and clear the screen
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.present();
+
+        Ok(Self {
+            context,
+            video_system,
+            canvas,
+        })
+    }
+
+    pub fn event_pump(&self) -> Result<EventPump, String> {
+        self.context.event_pump()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::path::Path;
+use std::thread;
+use std::time::Duration;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
 mod chip;
 use chip::Chip;
+
+mod display;
+use display::Display;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -15,4 +23,26 @@ fn main() {
 
     let mut chip = Chip::new();
     chip.load(rom).expect("Error while loading rom");
+
+    let display = &mut Display::init().expect("Error while initializing display");
+
+    let mut event_pump = display
+        .event_pump()
+        .expect("Error while getting event pump");
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        chip.execute_instruction()
+            .expect("Error while executing instruction");
+        thread::sleep(Duration::from_millis(2));
+    }
 }
